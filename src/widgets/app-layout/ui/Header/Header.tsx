f705b13ad699c211b57ef6db41e607ead80a08ad
Islam <isxmad@gmail.com>
2026-04-25 15:48:15 +0500

    feat: Header logo link to home page

--- src/widgets/app-layout/ui/Header/Header.tsx
+++ src/widgets/app-layout/ui/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { Logo } from "@shared/ui"
 import type { FC, PropsWithChildren } from "react"
+import { Link } from "react-router"
 
 export const Header:FC<PropsWithChildren> = ({children}) => {
   return (
     <div className="flex h-[55px] bg-white border-b">
-      <Logo disableHatchingBg/>
+      <Link className="flex" to={"/"}><Logo disableHatchingBg/></Link>
       {children}
     </div>
   )
